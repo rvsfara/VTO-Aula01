c19b77e9d39fd494bc360f1e6bf970b0002a94ab
rvsfara <rvsfara@gmail.com>
2022-10-26 09:13:14 -0300

    Operadores Matemáticos

---
+++ src/one/dio/basecamp/Main.java
@@ -0,0 +1,38 @@
+package one.dio.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+        //Declarando variáveis
+        int i;
+        int I; //recomendado usar letra minuscula e não nomes parecidos
+        //int _1a; //não recomendado usar _ no inicio de variáveis
+        int $aq;
+        int asrn24678md;
+        int asrn24678_md; //muito parecido o nome
+        //Inicializando as variáveis
+        i = 5;
+        I = 10;
+        $aq = 7;
+        final int j = 10; //variável final não pode ser modificada
+        asrn24678md = 100; //colocar nome que tenha significado e não assim
+        asrn24678_md = 150; //não usar nomes parecidos
+        int QUANTIDADE_OPCOES = 50;
+        final int NUMERO_TENTATIVAS = 5;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println($aq);
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn24678_md);
+        System.out.println(QUANTIDADE_OPCOES);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(NUMERO_TENTATIVAS);
+
+
+
+        System.out.println("Hello world!");
+
+    }
+}
